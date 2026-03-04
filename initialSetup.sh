465e285eebb08097c24e745b55c13ced792c3139
Carl <cd.kopf@gmail.com>
2026-03-04 16:13:02 -0500

    Create initialSetup.sh

---
+++ initialSetup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#Pull my aliases
+sudo curl -o ~/ https://raw.githubusercontent.com/carl61433/Linux-Files-and-Config/refs/heads/main/.bash_aliases
+
+# Add Docker's official GPG key:
+sudo apt update
+sudo apt install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Components: stable
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+sudo apt update
+
+#install docker engine
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
